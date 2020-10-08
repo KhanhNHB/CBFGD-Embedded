fd33e2b44672137e4c8adf21df0644c0a6050cb8
Khanhnhb <hello311016@gmail.com>
2020-10-08 15:37:51 +0700

    Push message to google Cloud use Pub/Sub

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
 const SerialPort = require("serialport");
 const SerialPortParser = require("@serialport/parser-readline");
 const GPS = require("gps");
-const Request = require("request-promise");
-
 const portGPS = new SerialPort("/dev/ttyAMA0", { baudRate: 9600 });
 const portArduino = new SerialPort("/dev/ttyACM0", { baudRate: 2000000 });
-
 const gps = new GPS();
-
 const parser = portGPS.pipe(new SerialPortParser());
+
+let lat = 0;
+let lon = 0;
+
+async function pushMessage(
+    lat,
+    lon,
+    speed,
+    topicName = 'projects/cbfgd-project/topics/gps_topic'
+) {
+    const { PubSub } = require('@google-cloud/pubsub');
+
+    const pubSubClient = new PubSub();
+    const data = JSON.stringify({lat: lat, lon: lon, speed: speed});
+
+    publishMessage = async () => {
+        const dataBuffer = Buffer.from(data);
+        await pubSubClient.topic(topicName).publish(dataBuffer);
+    }
+
+    publishMessage();
+}
 
 gps.on("data", async data => {
     if(data.type == "GGA") {
         if(data.quality != null) {
-            console.log(" [" + data.lat + ", " + data.lon + "]");
+            lat = data.lat;
+            lon = data.lon;
         } else {
             console.log("no gps fix available");
         }
@@ -36,7 +55,7 @@ function onOpen() {
 };
 
 function onData(data) {
-   console.log(new Date() + "-> " + data);
+    pushMessage(lat, lon, Buffer.from(data).toString());
 };
 
 portArduino.on('error', err => {
